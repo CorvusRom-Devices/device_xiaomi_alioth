92dfe91ff31907c5acffb20a6e39b467293b0feb
sukeeratsg <sukeeratgoindi@gmail.com>
2021-09-22 07:57:45 +0200

    Alioth : Corvus Bringup

--- corvus_alioth.mk
+++ corvus_alioth.mk
@@ -1,20 +1,19 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The corvus-OS
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
-# Inherit from alioth device
+# Inherit from device.
 
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-# Inherit some common Havoc-OS stuff.
+# Inherit from  corvusOS.
 
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
-# Device identifier. This must come after all inclusions.
+# Basic Target Flags 
 
-PRODUCT_NAME := havoc_alioth
+PRODUCT_NAME := corvus_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_BRAND := POCO
 PRODUCT_MANUFACTURER := Xiaomi
@@ -26,4 +25,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_SCREEN_DENSITY := 450
 TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-WITH_GMS := true
