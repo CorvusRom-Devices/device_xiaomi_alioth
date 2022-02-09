ff5e92588a83bf8a24fa253bb8e4ab3a9378df4d
cristianvaz <cristianvaz81993@gmail.com>
2022-02-09 10:23:40 +0100

    alioth: Blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/bliss_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    bliss_alioth-user \
+    bliss_alioth-userdebug \
+    bliss_alioth-eng
